ee668e3d884a5291ad5dfdecae3ee7d805269b81
shishkiny <shishkin@24ttl.net>
2026-03-28 14:54:56 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,30 +27,3 @@ export default function Home() {
     </>
   );
 }
-import { Header } from "@/components/sections/header";
-import { Hero } from "@/components/sections/hero";
-import { HowItWorks } from "@/components/sections/how-it-works";
-import { WhyThisMatters } from "@/components/sections/why-this-matters";
-import { ItemSpec } from "@/components/sections/item-spec";
-import { MiniApp } from "@/components/sections/mini-app";
-import { Benefits } from "@/components/sections/benefits";
-import { Trust } from "@/components/sections/trust";
-import { FinalCTA } from "@/components/sections/final-cta";
-import { Footer } from "@/components/sections/footer";
-
-export default function Home() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <HowItWorks />
-      <WhyThisMatters />
-      <ItemSpec />
-      <MiniApp />
-      <Benefits />
-      <Trust />
-      <FinalCTA />
-      <Footer />
-    </>
-  );
-}
